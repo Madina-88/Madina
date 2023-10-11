44504f53e7a87f0fccd6fe3d19488fe148d8cb73
Madina-88 <ergasevamalika066.gmail.com>
2023-10-11 18:02:32 +0300

    AAAA

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Madina");
         System.out.println("asad");
+        System.out.println("asdsad");
         }
     }
